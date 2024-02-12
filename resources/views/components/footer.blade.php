72b95543f29f031f390b61a5af03c9deaa159c12
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2024-02-12 17:45:11 +0100

    update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,22 @@
 <footer>
-    <h1>Hello from footer</h1>
+    <div class="container-fluid bg-primary">
+        <div class="container">
+            <div class="row justify-content-space-between mt-3">
+                <div class="row row-cols-6">
+                    <button class="bg-transparent border-0 text-light">
+                        DIGITAL COMICS
+                    </button>
+                    <button class="bg-transparent border-0 text-light">
+                        DC MERCHANDISE
+                    </button>
+                    <button class="bg-transparent border-0 text-light">
+                        SUBSCRIPTION
+                    </button>
+                    <button class="bg-transparent border-0 text-light">
+                        COMIC SHOP LOCATOR
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,30 @@
 <header>
-    <h1>Hello from Header</h1>
+    <div class="container">
+        <div class="row justify-content-space-between mt-3">
+            <div class="col-5">
+                <img src="{{ Vite::asset('resources/imgs/dc-logo.png') }}" width="50px"/>
+            </div>
+            <div class="col-7">
+                <div class="row justify-content-center">
+                    <div class="col-10">
+                        <button class="bg-transparent border-0">
+                            CHARACTERS
+                        </button>
+                        <button class="bg-transparent border-0">
+                            COMICS
+                        </button>
+                        <button class="bg-transparent border-0">
+                            MOVIES
+                        </button>
+                        <button class="bg-transparent border-0">
+                            TV
+                        </button>
+                    </div>
+                    <div class="col-2 mt-3"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid jumbotron">
+    </div>
 </header>
